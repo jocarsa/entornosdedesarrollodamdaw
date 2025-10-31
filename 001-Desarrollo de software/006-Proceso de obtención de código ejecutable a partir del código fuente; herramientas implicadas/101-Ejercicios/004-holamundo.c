650517d740464b008db9a812a5cc49cb473b040e
Jose Vicente Carratala <info@josevicentecarratala.com>
2025-10-31 09:16:57 +0100

    1

---
+++ 001-Desarrollo de software/006-Proceso de obtención de código ejecutable a partir del código fuente; herramientas implicadas/101-Ejercicios/004-holamundo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// Include = Traigo librería
+// std = standard (la de por defecto)
+// i = input (entradas)
+// o = output (salidas)
+// .h indica librería de C
+
+int main(){
+  printf("Hola mundo desde C");
+  return 0;
+}
+// Main es la funcion principal
+// int es el tipo de datos que devuelve la funcion
+// return 0 cumple la promesa
+// printf equivale a print
+// En C las instrucciones finalizan con ;
---
+++ 001-Desarrollo de software/006-Proceso de obtención de código ejecutable a partir del código fuente; herramientas implicadas/101-Ejercicios/007-hola mundo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// Include = traigo librería
+// i = input
+// o = output
+// stream = flujo
+
+using namespace std;
+// Uso un espacio de nombres
+
+int main(){
+  cout << "Hola mundo en C++";
+}
+
+// compilar: g++ 007-hola\ mundo.cpp -o acpp.out
+
